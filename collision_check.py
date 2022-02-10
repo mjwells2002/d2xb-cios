049e9830982c8606c02fc669dbb5632e2d8f921c
Andrew Piroli <drewpiroli@gmail.com>
2022-02-10 14:19:31 +0000

    Tools: impove collision check script
    It was kind of crappy before because I rewrote part of it halfway through
    but never really finished the job. This simplifies operation a lot and increases
    execution speed. Also, it now outputs collisions in a more structured (normal)
    format using tabs for alignment. This will allow the output to be parsed by other
    tools more easily.

--- collision_check.py
+++ collision_check.py
@@ -13,12 +13,6 @@ class Section:
     tag: str = dataclasses.field(default="Unknown!")
 
 
-@dataclasses.dataclass(frozen=True, order=True)
-class DotApp:
-    filename: str
-    sections: List[Section] = dataclasses.field(default_factory=list)
-
-
 # Regex for capturing find output with the filename
 # Example input:
 # "File: ./relative/path/to/a/file"
@@ -60,8 +54,8 @@ def any_return(a: Iterable[Tuple[bool, Section, Section]]) -> Union[Section, boo
 
 
 # This is where the fun begins
-processed_readelf_output: Dict[str, DotApp] = dict()
 in_file: Union[None, str] = None
+all_sections: List[Section] = list()
 
 # Expects output from `find ... -exec readelf -l ...` on stdin
 for readelf_line in sys.stdin:
@@ -69,7 +63,6 @@ for readelf_line in sys.stdin:
     # keep track of that so we know what readelf output is in each file
     if match := start_of_record.search(readelf_line):
         in_file = match.group(1)
-        processed_readelf_output.update({in_file: DotApp(in_file)})
         continue
     elif (
         in_file
@@ -84,32 +77,30 @@ for readelf_line in sys.stdin:
             ):  # Can safely ignore this since they all have a load at 0x0
                 continue
             # Store them for later processing
-            processed_readelf_output[in_file].sections.append(
-                Section(physaddr, memsiz, in_file)
-            )
+            all_sections.append(Section(physaddr, memsiz, in_file))
     else:
         # Any other output, we don't care about
         pass
 
 found = False
-all_sections: List[Section] = list()
+checked_sections: List[Section] = list()
 
 # Loop over everything we got
-# The keys were just filenames for easy access in the stdin parsing phase
-# We don't need them anymore since there's an extra copy of that stashed in Section() dataclass
-for app in processed_readelf_output.values():
-    # loop over all sections belonging to a file
-    for section in app.sections:
-        # check if any of those sections overlap with any section in the all_sections list
-        # if so, assign it to first_overlap so it can be printed to the screen
-        # Yes this is a triple nested for loop inside any_return(), I don't care - it works (probably)
-        if first_overlap := any_return(
-            [overlaps(previous_sections, section) for previous_sections in all_sections]
-        ):
-            print(
-                f"Collision detected! File: {app.filename} Section: {section} Overlapped with {first_overlap}"
-            )
-            found = True
-        all_sections.append(section)
+for section in all_sections:
+    # check if any of those sections overlap with any section we have already checked
+    # if so, assign it to first_overlap so it can be printed to the screen
+    if first_overlap := any_return(
+        [overlaps(previous_sections, section) for previous_sections in checked_sections]
+    ):
+        # I guess dataclasses don't play very nice with f-strings :(
+        our_start = section.startaddr
+        our_size = section.size
+        collided_start = first_overlap.startaddr
+        collided_size = first_overlap.size
+        print(
+            f"Collision detected! File: {section.tag}\t Addr: {our_start:#08x} Size: {our_size:#08x}\tOverlapped with {first_overlap.tag}\t{collided_start:#08x} {collided_size:#08x}"
+        )
+        found = True
+    checked_sections.append(section)
 if not found:
     print("No collisions detected! PogYou")
